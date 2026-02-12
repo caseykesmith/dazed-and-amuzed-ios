7aaf9e9af33aff42fa046d5e6edb876e02fcb9fd
Peyton Sadler <sadlerpeyton@gmail.com>
2026-02-11 21:43:54 -0500

    Initial Commit

---
+++ DazedAmuzed/DazedAmuzedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DazedAmuzedApp.swift
+//  DazedAmuzed
+//
+//  Created by Peyton Sadler on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DazedAmuzedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
